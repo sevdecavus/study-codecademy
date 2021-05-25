6232bcab1f1578d5861ac898ce4832ee84139997
Sevde Cavus <60116628+sncavus@users.noreply.github.com>
2021-05-25 17:03:10 -0400

    Update script_secretMessage.js

--- script_secretMessage.js
+++ script_secretMessage.js
@@ -1,3 +1,9 @@
+/*
+Secret Message
+Using array methods, you will transform an array of strings into a secret message!
+*/
+
+
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 secretMessage.pop()
 //console.log(secretMessage)
